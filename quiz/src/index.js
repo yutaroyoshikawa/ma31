3819bf35a7aeb7ef24976e18112c6c0c588e8453
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2019-11-29 11:22:22 +0900

    function componentに書き換え

--- quiz/src/index.js
+++ quiz/src/index.js
@@ -1,89 +1,81 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
 let data = require("./data.json");
 
-class Answer extends React.Component {
-  render() {
-    return (
-      <li
-        key={this.props.key}
-        onClick={() => this.props.clickItem(this.props.text)}
-      >
-        {this.props.text}
-      </li>
-    );
-  }
+const Answer = props => {
+  return (
+    <li
+      key={props.key}
+      onClick={() => props.clickItem(props.text)}
+    >
+      {props.text}
+    </li>
+  )
 }
 
-class CorrectAnswer extends React.Component {
-  render() {
-    return (
-      <div className="sample">
-        <div>{this.props.is_correct ? "正解" : "不正解"}</div>
-        <div>答え: {this.props.correct_text}</div>
-      </div>
-    );
-  }
+const CorrectAnswer = props => {
+  return (
+    <div className="sample">
+      <div>{props.is_correct ? "正解" : "不正解"}</div>
+      <div>答え: {props.correct_text}</div>
+    </div>
+  )
 }
-class Quiz extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      quiz_list: this.props.data,
-      number: 0,
-      is_answer: false,
-      is_correct: false
-    };
-  }
-  render() {
-    let quiz = this.state.quiz_list[this.state.number];
 
-    return (
-      <section>
-        <h1> {quiz.title} </h1>
-        <ul>
-          {quiz.answer.map(item => {
-            return (
-              <Answer
-                key={item}
-                text={item}
-                clickItem={choice => this.answer(choice)}
-              />
-            );
-          })}
-        </ul>
-        {this.state.is_answer && (
-          <CorrectAnswer
-            is_correct={this.state.is_correct}
-            correct_text={quiz.answer[quiz.correct]}
-          />
-        )}
-        <button onClick={() => this.next()}>次へ</button>
-      </section>
-    );
-  }
+const Quiz = props => {
+  const [quizList, setQuizList] = useState(props.data);
+  const [pageNumber, setPageNumber] = useState(0);
+  const [isAnswer, setIsAnswer] = useState(false);
+  const [isCorrect, setIsCorrect] = useState(false);
 
-  answer(choice) {
-    let quiz = this.state.quiz_list[this.state.number];
-    let choice_index = quiz.answer.indexOf(choice);
-    this.setState({
-      quiz_list: this.props.data,
-      number: 0,
-      is_answer: true,
-      is_correct: choice_index === quiz.correct
-    });
-  }
-  next() {
-    alert("hoge");
-    this.setState({
-      quiz_list: this.props.data,
-      number: this.state.number + 1,
-      is_answer: true,
-      is_correct: choice_index === quiz.correct
-    });
-  }
+  const initComponent = useCallback(() => {
+    setQuizList(props.data);
+    setIsAnswer(false);
+    setIsCorrect(false);
+  }, [props.data]);
+
+  const answer = (choice) => {
+    const quiz = quizList[pageNumber];
+    const choice_index = quiz.answer.indexOf(choice);
+
+    setIsAnswer(true);
+    setIsCorrect(choice_index === quiz.correct);
+  };
+
+  const next = () => {
+    setPageNumber(pageNumber + 1);
+    initComponent();
+  };
+
+  return (
+    <section>
+      <h1> {quizList[pageNumber].title} </h1>
+      <ul>
+        {quizList[pageNumber].answer.map(item => {
+          return (
+            <Answer
+              key={item}
+              text={item}
+              clickItem={choice => answer(choice)}
+            />
+          );
+        })}
+      </ul>
+      {isAnswer && (
+        <CorrectAnswer
+          is_correct={isCorrect}
+          correct_text={quizList[pageNumber].answer[quizList[pageNumber].correct]}
+        />
+      )}
+      {
+        quizList.length - 1 !== pageNumber && (
+          <button onClick={() => next()}>次へ</button>
+        )
+      }
+    </section>
+  )
 }
 
 ReactDOM.render(<Quiz data={data.quiz} />, document.getElementById("root"));
